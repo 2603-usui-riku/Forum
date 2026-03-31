d4d0636aabf059a1acb8ced3cee42144cf1ed294
usui_riku <usui.riku@alhinc.jp>
2026-03-31 16:36:31 +0900

    add/delete:板、スレッド、投稿の三段階の機能追加のために必要なファイルを追加・削除

--- db/create_table_20260330.sql
+++ db/create_table_20260330.sql
@@ -7,7 +7,7 @@ CREATE TABLE boards (
 );
 
 -- 2. 話題をまとめる「スレッド」
-CREATE TABLE threads (
+CREATE TABLE topics (
     id SERIAL PRIMARY KEY,
     board_id INTEGER NOT NULL REFERENCES boards(id) ON DELETE CASCADE,
     title VARCHAR(100) NOT NULL,
@@ -19,13 +19,11 @@ CREATE TABLE threads (
 -- 3. 実際の書き込み「ポスト」
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
-    thread_id INTEGER NOT NULL REFERENCES threads(id) ON DELETE CASCADE,
+    topic_id INTEGER NOT NULL REFERENCES topics(id) ON DELETE CASCADE,
     post_num INTEGER NOT NULL,
-    name VARCHAR(50) DEFAULT '名無しさん',
     content TEXT NOT NULL,
-    del_password VARCHAR(255),
-    ip_address VARCHAR(45),
-    is_edited BOOLEAN DEFAULT FALSE,
+    is_edited BOOLEAN DEFAULT FALSE NOT NULL,
+    is_deleted BOOLEAN DEFAULT FALSE NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
